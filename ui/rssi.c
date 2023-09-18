e4e02f610b9a49b9a5cd932c3b48efd948779629
manu <a@b.c>
2023-09-18 13:21:11 +0200

    rssi things

--- ui/rssi.c
+++ ui/rssi.c
@@ -22,52 +22,8 @@
 #include "settings.h"
 #include "ui/rssi.h"
 #include "ui/ui.h"
+#include "helper.h"
 
-static void Render(uint8_t RssiLevel, uint8_t VFO)
-{
-	uint8_t *pLine;
-	uint8_t Line;
-	bool bIsClearMode;
-
-	if (gCurrentFunction == FUNCTION_TRANSMIT || gScreenToDisplay != DISPLAY_MAIN) {
-		return;
-	}
-
-	if (VFO == 0) {
-		pLine = gFrameBuffer[2];
-		Line = 3;
-	} else {
-		pLine = gFrameBuffer[6];
-		Line = 7;
-	}
-
-	memset(pLine, 0, 23);
-	if (RssiLevel == 0) {
-		pLine = NULL;
-		bIsClearMode = true;
-	} else {
-		memcpy(pLine, BITMAP_Antenna, 5);
-		memcpy(pLine + 5, BITMAP_AntennaLevel1, sizeof(BITMAP_AntennaLevel1));
-		if (RssiLevel >= 2) {
-			memcpy(pLine + 8, BITMAP_AntennaLevel2, sizeof(BITMAP_AntennaLevel2));
-		}
-		if (RssiLevel >= 3) {
-			memcpy(pLine + 11, BITMAP_AntennaLevel3, sizeof(BITMAP_AntennaLevel3));
-		}
-		if (RssiLevel >= 4) {
-			memcpy(pLine + 14, BITMAP_AntennaLevel4, sizeof(BITMAP_AntennaLevel4));
-		}
-		if (RssiLevel >= 5) {
-			memcpy(pLine + 17, BITMAP_AntennaLevel5, sizeof(BITMAP_AntennaLevel5));
-		}
-		if (RssiLevel >= 6) {
-			memcpy(pLine + 20, BITMAP_AntennaLevel6, sizeof(BITMAP_AntennaLevel6));
-		}
-		bIsClearMode = false;
-	}
-
-	ST7565_DrawLine(0, Line, 23 , pLine, bIsClearMode);
-}
 
 void UI_UpdateRSSI(uint16_t RSSI)
 {
@@ -87,7 +43,28 @@ void UI_UpdateRSSI(uint16_t RSSI)
 
 	if (gVFO_RSSI_Level[gEeprom.RX_CHANNEL] != Level) {
 		gVFO_RSSI_Level[gEeprom.RX_CHANNEL] = Level;
-		Render(Level, gEeprom.RX_CHANNEL);
 	}
+
+    if (gCurrentFunction == FUNCTION_TRANSMIT || gScreenToDisplay != DISPLAY_MAIN) {
+        return;
+    }
+
+    //do the calculation
+    int16_t realRSSI = (int16_t)((RSSI >> 1) & 0xFF);
+    realRSSI = realRSSI - 160;
+    realRSSI *= -1;
+
+    //rssi to string
+    char rssi_str[8];
+    NUMBER_ToDigits(realRSSI, rssi_str);
+
+    //which line
+    uint8_t line = gEeprom.RX_CHANNEL == 0 ? 2 : 6;
+    //print the rssi
+    UI_DisplaySmallDigits(3,rssi_str+5, 2, line);
+    //print the minus
+    gFrameBuffer[line][0] = 0x10;
+    gFrameBuffer[line][1] = 0x10;
+    ST7565_BlitFullScreen();
 }
 
