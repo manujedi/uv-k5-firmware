6fd344ce07ea88f5a773c679b0d9a252e1c605bd
manu <a@b.c>
2023-09-21 16:50:35 +0200

    fix comment

--- driver/bk4819-regs.h
+++ driver/bk4819-regs.h
@@ -1,6 +1,6 @@
 /* Copyright 2023 Dual Tachyon
  * https://github.com/DualTachyon
- * /* Copyright 2023 Manuel Jedinger
+ * Copyright 2023 Manuel Jedinger
  * https://github.com/manujedi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
